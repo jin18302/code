7a7ebca1d003b0b0d3f4bedf62d605f19b0f48ff
jin18302 <jin18302@naver.com>
2024-11-12 13:20:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.93 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,13 +1,11 @@
 class Solution {
     public String solution(String[] seoul) {
-        int index=0;
-        while(true){
-            
-            if("Kim".equals(seoul[index]))
+        int index = 0;
+        while (true) {
+            if ("kim".equalsIgnoreCase(seoul[index]))
                 break;
-            
             index++;
         }
-            return "김서방은 "+index+"에 있다";
+        return "김서방은 " + index + "에 있다";
         }
     }
