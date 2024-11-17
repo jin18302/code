2b6455d3485e94f20e7599db4fafe91cd1950a77
jin18302 <jin18302@naver.com>
2024-11-17 22:19:45 +0900

    [level 1] Title: 음양 더하기, Time: 0.08 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        for(int i=0;i<signs.length;i++){
+            if(signs[i]==false){
+                absolutes[i]= -absolutes[i];
+            }
+        }
+        
+        int sum=0;
+        for(int i=0;i<absolutes.length;i++){
+            sum+=absolutes[i];
+        }
+        return sum;
+            
+    }
+}
