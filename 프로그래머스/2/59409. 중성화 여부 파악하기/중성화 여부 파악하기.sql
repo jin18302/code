9ef31896e60be3519c75573b2846ac771e8104e8
jin18302 <jin18302@naver.com>
2024-11-18 09:56:44 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,5 +1,4 @@
-SELECT ANIMAL_ID, 
-       NAME, 
-       IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%', 'O', 'X') AS IS_NEUTERED
-FROM animal_ins
-ORDER BY ANIMAL_ID;
+select animal_id, name, 
+if(SEX_UPON_INTAKE like "%Neutered%" or SEX_UPON_INTAKE like "%Spayed%", 'O', 'X')as '중성화'
+from animal_ins
+order by  animal_id;
