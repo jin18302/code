b791a3db0cc6c0ab137c6edc6cc9e618aaaedeb0
jin18302 <jin18302@naver.com>
2025-03-15 23:52:46 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.10 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,23 +1,12 @@
-import java.util.*;
-import java.util.stream.*;
+
 class Solution {
     public String solution(int[] food) {
-        String result = "";
-        LinkedList<Character> linkedList = new LinkedList<>();
-
-        for (int i = 1; i < food.length; i++) {//배열의 길이만큼 반복한다
-            if (food[i] % 2 != 0) {
-                // 만약 인덱스[i]가 가지고 있는 값이 홀수이면 양수로 만들어준다
-                food[i] -= 1;
-            }
-            for (int j = 0 ; j < food[i]; j++) {//인덱스[i]가 가지고있는 수만큼 반복한다
-                linkedList.add(linkedList.size() / 2, (char) ((char) i + '0'));
-            }
+        StringBuilder builder = new StringBuilder();
+        for (int i=1; i<food.length; i++) {
+            int result = food[i] / 2;
+            builder.append(String.valueOf(i).repeat(result));
         }
-        linkedList.add(linkedList.size() / 2, '0');
-        
-         return linkedList.stream()
-                .map(String::valueOf) 
-                .collect(Collectors.joining());
+        String answer = builder + "0";
+        return answer + builder.reverse();
     }
 }
